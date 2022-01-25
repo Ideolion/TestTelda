56bf5aa2c154e654d4a99a65d5d6eaf6004238c9
Ideolion <skegul24@gmail.com>
2022-01-25 18:53:00 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# TestTelda
+ TestTelda
---
+++ src/main/java/com/example/TestTelda/TestTeldaApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestTelda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestTeldaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestTeldaApplication.class, args);
+	}
+
+}
